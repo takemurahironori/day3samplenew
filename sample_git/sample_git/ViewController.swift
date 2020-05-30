6e94c2c9193d270b33b7a568f2afaa63f55ccf56
Your Name <you@example.com>
2020-05-30 21:02:46 +0900

    test_git 2020/05/29

---
+++ sample_git/sample_git/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sample_git
+//
+//  Created by 竹村博徳 on 2020/05/30.
+//  Copyright © 2020 竹村博徳. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
